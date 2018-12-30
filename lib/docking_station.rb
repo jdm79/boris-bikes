484bf7f885d5384b03d8b781228f0fb7590f3217
jdm79 <james.malvern@gmail.com>
2018-12-30 13:10:14 +0000

    test: creates test to raise error if no bike available when trying to release a bike, passing code

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,8 +2,12 @@ require_relative 'bike'
 
 class DockingStation
   attr_reader :bike
+ 
   def release_bike
-    Bike.new
+    # Guard condition pattern
+    # By catching errors is first priority, errors cannot progress further into program
+    fail 'No bikes available' unless @bike
+    @bike
   end
 
   def dock(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,17 +3,24 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
+  describe '#release bike' do
+    it 'releases working bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
 
+    it 'raises an error when dock is empty' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+  
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   it { is_expected.to respond_to(:bike) }
 
   it 'docks something' do
-    bike = Bike.new 
+    bike = Bike.new
     expect(subject.dock(bike)).to eq(bike)
   end
 
@@ -22,5 +29,4 @@ describe DockingStation do
     subject.dock(bike)
     expect(subject.bike).to eq(bike)
   end
-
 end
